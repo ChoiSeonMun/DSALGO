4fa33a3eb99391507a7d7b53640f854b15750d8a
smchoi <seonmun.choi@gmail.com>
2021-10-12 17:40:01 +0900

    임시 커밋

--- DSALGO/List/DoubleLinkedList.h
+++ DSALGO/List/DoubleLinkedList.h
@@ -1,74 +1,88 @@
 #pragma once
 
-class SingleLinkedList
+class DoubleLinkedList
 {
 public:
 	struct Node
 	{
-		Node(int data = 0, Node* next = nullptr);
+		Node(int data = 0, Node* prev = nullptr, Node* next = nullptr)
+			: Data{ data }, Next{ next }, Prev{ prev } { }
 		Node(const Node&) = delete;
 		Node& operator=(const Node&) = delete;
-		~Node();
+		~Node() = default;
 
 		int		Data = 0;
 		Node*	Next = nullptr;
+		Node*	Prev = nullptr;
 	};
 
 public:
 	// 기본 생성자
-	SingleLinkedList();
+	DoubleLinkedList() = default;
 
 	// count만큼의 요소를 갖고 있는 컨테이너를 만드는 생성자
-	explicit SingleLinkedList(size_t count);
+	explicit DoubleLinkedList(size_t count);
 
 	// 복사 생성자.
-	SingleLinkedList(const SingleLinkedList& other);
+	DoubleLinkedList(const DoubleLinkedList& other);
 
 	// 할당 연산자
-	SingleLinkedList& operator=(const SingleLinkedList& rhs);
-	
+	DoubleLinkedList& operator=(const DoubleLinkedList& rhs);
+
 	// 소멸자
-	~SingleLinkedList();
+	~DoubleLinkedList();
 
 	// 첫 번째 요소를 반환한다.
 	int&			front();
 	const int&		front() const;
 
-	// 시작 전 요소를 가리키는 반복자를 반환한다.
-	Node*			before_begin();
-	const Node*		before_begin() const;
-	
+	// 마지막 요소를 반환한다.
+	int&			back();
+	const int&		back() const;
+
 	// 시작 요소를 가리키는 반복자를 반환한다.
+	// 리스트가 비어있다면 end()와 같다.
 	Node*			begin();
 	const Node*		begin() const;
-	
+
 	// 끝 다음 요소를 가리키는 반복자를 반환한다.
 	Node*			end();
 	const Node*		end() const;
 
 	// pos 다음에 value를 삽입한다.
 	// 삽입된 요소를 가리키는 반복자를 반환한다.
-	Node*			insert_after(Node* pos, int value);
-	
+	Node*			insert(Node* pos, int value);
+
 	// pos 다음 요소를 삭제한다.
 	// 삭제된 요소의 다음 요소를 가리키는 반복자를 반환한다.
 	// 아무 요소도 없으면 end()를 반환한다.
-	Node*			erase_after(Node* pos);
+	Node*			erase(Node* pos);
 
-	// 시작 요소에 value를 삽입한다.
+	// 시작에 value를 삽입한다.
 	void			push_front(int value);
-	
+
+	// 끝에 value를 삽입한다.
+	void			push_back(int value);
+
 	// 시작 요소를 제거한다.
 	void			pop_front();
 
+	// 끝 요소를 제거한다.
+	void			pop_back();
+
 	// 컨테이너가 비었는지 판단한다.
 	bool			empty() const;
+	
+	// 리스트 안에 있는 요소의 개수를 반환한다.
+	size_t			size() const;
 
 	// 컨테이너를 비워버린다.
 	void			clear();
-	
+
+	// 해당 value가 있는지 체크한다.
 	bool			contains(int value) const;
 private:
-	Node*			_head = new Node();
 	Node*			_end = new Node();
+	Node*			_head = _end;
+	size_t			_size = 0;
 };
---
+++ DSALGO/List/List.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <list>
+
+#include "DoubleLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	list<int> lst;
+
+	lst.push_front(1);
+	lst.push_front(3);
+	lst.push_back(2);
+
+	for (int elem : lst)
+	{
+		cout << elem << ' ';
+	}
+	cout << endl;
+
+	lst.pop_back();
+	
+	cout << lst.front() << endl;
+	cout << lst.back() << endl;
+
+	list<int> list2(5);
+	auto iter = list2.insert(list2.begin(), 3);
+	++iter;
+	iter = list2.insert(iter, 4);
+	list2.erase(iter);
+
+	for (int elem : list2)
+	{
+		cout << elem << ' ';
+	}
+	cout << endl;
+
+	cout << boolalpha << list2.empty() << endl;
+
+	list<int> list3(lst);
+	for (int elem : list3)
+	{
+		cout << elem << ' ';
+	}
+	cout << endl;
+
+	DoubleLinkedList myList;
+
+	myList.push_front(1);
+	myList.push_front(3);
+	myList.push_back(2);
+
+	for (auto iter = myList.begin(); iter != myList.end(); iter = iter->Next)
+	{
+		cout << iter->Data << endl;
+	}
+}
